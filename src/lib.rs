bba6dd85842250d9fb97988a43f9a1415775563a
toginn <89682463+toginn@users.noreply@github.com>
2022-01-29 22:01:53 +0900

    Add: use search in run fn

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,9 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
 
-    // ファイルの表示
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
 }
