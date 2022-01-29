bfb7e8e487c0d2c032416ddf7c1486462ddd9c26
toginn <89682463+toginn@users.noreply@github.com>
2022-01-29 17:04:36 +0900

    Add: comment

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,23 @@ use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
+    // 引数の解析
     let args: Vec<String> = env::args().collect();
 
     let query = &args[1];
     let filename = &args[2];
 
+    // 引数の表示
     println!("Searching for {}", query);
     println!("In file {}", filename);
 
+    // ファイルの解析
     let mut f = File::open(filename).expect("file not found");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)
         .expect("something went wrong reading the file");
 
+    // ファイルの表示
     println!("With text:\n{}", contents);
 }
