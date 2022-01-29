a4f7d31a9e416ce4f68711a0ef8b4f19986604a2
toginn <89682463+toginn@users.noreply@github.com>
2022-01-29 17:41:06 +0900

    Refactor: replace to config constructor

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     // 引数の解析
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     // 引数の表示
     println!("Searching for {}", config.query);
@@ -28,9 +28,11 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
 
-    Config { query, filename }
+        Config { query, filename }
+    }
 }
